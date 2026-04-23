ca9e4490b5a0c66ad25b979d7d4351bfcd754eb5
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-04-23 17:56:48 -0300

    docs: make project reuse compliant

--- src/consumo/lib/classes.py
+++ src/consumo/lib/classes.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2026 Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
+#
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """Module containing classes for abstraction."""
--- src/consumo/lib/file/__init__.py
+++ src/consumo/lib/file/__init__.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2026 Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
+#
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """File Backend of the Content Consumption Analyzer."""
